46c36a091e5c7c324426373880f6dc64bbd97b8a
IgorFesenko <43293499+IgorFesenko@users.noreply.github.com>
2024-02-15 16:05:20 -0500

    Update shuffler_intergenic.sh

--- shuffler_intergenic.sh
+++ shuffler_intergenic.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 # -*- coding: utf-8 -*-
 
-# Creating a set of shufled sequences
+# Creating a set of shufled intergenic sequences
 
-FASTA="/panfs/pan1/small_orf/intergenic"
+FASTA="path-to-directory-with-fasta"
 
 while read -r line; do
 
     echo "Shuffle sequnces from $line"
-    #python3 shuffler.py --file $FASTA/$line.intergenic.fa
     python ./shuffler_updated.py --file $FASTA/$line/$line.intergenic.fa --dir ./temp
     echo "Done"
 done < $1

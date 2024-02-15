ef2695406107715ef9eba2f9d54ec4e60300bf0d
IgorFesenko <43293499+IgorFesenko@users.noreply.github.com>
2024-02-15 17:17:06 -0500

    Update fastqc_ribo.sh

--- Ribo-seq/fastqc_ribo.sh
+++ Ribo-seq/fastqc_ribo.sh
@@ -11,5 +11,5 @@ echo "Performing FASTQC analysis of raw reads quality"
 while read -r line; do
     fastqc -t 4 -o $PWD/$line/fastqc_cleaned $PWD/$line/cleaned/* # запускаем fastqc
     multiqc $PWD/$line/fastqc_cleaned -o $PWD/$line/fastqc_cleaned # объединяем отчеты при помощи multiqc
-    cp $PWD/$line/fastqc_cleaned/multiqc_report.html /home/fesenkoi2/"$line"_cleaned_multiqc_report.html
+    
 done < $1

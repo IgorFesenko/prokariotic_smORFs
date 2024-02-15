7bef439d9e539d48c828b179b50172540c1e10b4
IgorFesenko <43293499+IgorFesenko@users.noreply.github.com>
2024-02-15 16:39:58 -0500

    Update DE_Yersinia.R

--- DE_RNAseq/DE_Yersinia.R
+++ DE_RNAseq/DE_Yersinia.R
@@ -1,9 +1,8 @@
-library(edgeR)  # загрузка библиотеки
+library(edgeR) 
 rm(list=ls())
-setwd('/Users/fesenkoi2/Library/CloudStorage/OneDrive-Personal/SmORFs/raw_tables/DE_RNAseq')
+setwd('DE_RNAseq')
 
 
-# загружаем данные которые сохранили в прошлый раз
 counts <-  readRDS('countsYersinia.Rdata')
 head(counts)
 
@@ -13,7 +12,6 @@ head(counts)
 # genes without expression
 table(apply(counts,1,mean) == 0)
 
-#задаем метаданные для образцов
 meta <-  data.frame(conditions=c("AceticAcid", "AceticAcid", "AceticAcid", 
                                  "BileSalt", "BileSalt", "BileSalt",
                                  "Control", "Control","Control",
@@ -30,36 +28,22 @@ rownames(meta) = colnames(counts)
 table(meta)
 meta
 
-## Загружаем данные в edgeR и нормализуем
-edger <-  DGEList(counts = counts) # создаем объект edgeR хранящий каунты
+edger <-  DGEList(counts = counts) 
 
-#фильтруем по покрытию
-#keep <- filterByExpr(edger)
 keep <- rowSums(cpm(edger) > 0.5) >= 3
-#fc10 = fc[apply(fc,1,mean) >= 10,]
 table(keep)
 edger <- edger[keep, , keep.lib.sizes=FALSE]
 
-#fc10 = fc[apply(fc,1,mean) >= 10,]
-#table(apply(edger,1,mean) >= 10) 
-
-
-# нормируем методом TMM
 edger = calcNormFactors(edger,method='TMM')
 edger$samples
-
-#создаем матрицу дизайна
 
 design <- model.matrix(~0+conditions, data = meta)
 design
 
-# Сохраняем промежуточные данные
 saveRDS(design,'designYersinia.Rdata') 
 
 
 # biological coefficient of variation (BCV)
-# рисуем зависимость биологической вариабельности от средней экспрессии, 
-#немного падает с ростом экпсрессии
 edger <-  estimateDisp(edger,design)
 plotBCV(edger)
 edger$common.dispersion

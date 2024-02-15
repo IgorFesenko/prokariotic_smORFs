8db10b1b8a175c680703c75d34e95e9080eb5f70
IgorFesenko <43293499+IgorFesenko@users.noreply.github.com>
2024-02-15 16:23:22 -0500

    Update featureCounts.sh

--- RNAseq/featureCounts.sh
+++ RNAseq/featureCounts.sh
@@ -12,9 +12,9 @@ while read -r line; do
     echo "$line"
     if [ "$2" = "PE" ]; then
         featureCounts -T 10 -p --countReadPairs -t gene,smORF,exon,ncRNA,rRNA,tRNA,tmRNA -g ID -a $PWD/$line/$line*.gff -o $PWD/$line/"$line"_FeatureCounts $PWD/$line/bam/Combined.bam
-	cp $PWD/$line/"$line"_FeatureCounts /home/fesenkoi2
+	
     else
         featureCounts -T 10 -a $PWD/$line/$line*.gff -t gene,smORF,exon,ncRNA,rRNA,tRNA,tmRNA -g ID -o $PWD/$line/"$line"_FeatureCounts $PWD/$line/bam/Combined.bam
-	cp $PWD/$line/"$line"_FeatureCounts /home/fesenkoi2
+	
     fi
 done < $1
